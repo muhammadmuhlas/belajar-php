2c75cd866d9259aa9ec7bb02a61101fd55640b3d
Muhammad Muhlas <muhammadmuhlas3@gmail.com>
2017-02-24 20:27:24 +0700

    Adding Hendrity

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// echo "Hello World";
+$nama = "Muhammad Muhlas";
+$umur = 19;
+$ipk = 3.96;
+
+echo "Halo, namaku $nama " . ' umur temanku $umur' . " IPK dosen ku " . $ipk;
+
+$var = 5;
+
+echo "\n";
+
+if ($ipk  === 3.96){
+	echo "Hendrity sejati";
+}
+
+?>
